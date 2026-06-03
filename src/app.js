d9b140492f9838ea97314ab55ad3fd9538f049f3
MehmetGktrk <mehmetgokturk1901@gmail.com>
2026-06-03 15:46:03 +0300

    feat(error-handling): add error utility and middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use(errorMiddleware);
 
 module.exports = app;
